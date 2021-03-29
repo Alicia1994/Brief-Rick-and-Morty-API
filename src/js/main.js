c32290774423da2da42df3edbe62dd501d5bdd6f
Alicia <aliciaderradji@live.fr>
2021-03-29 12:17:45 +0200

    add modifs JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,3 @@
-// let urls = ["https://rickandmortyapi.com/api/character", "https://rickandmortyapi.com/api/character/?page=2", "https://rickandmortyapi.com/api/character/?page=3"];
-// for (let index = 0; index < urls.length; index++) {
-//     const url = urls[index];
-// }
-// Promise.all(urls.map(url => fetch(url)))
-//     .then(resp => Promise.all(resp.map(r => r.json())))
-
 window.onload = LoadLocation;
 
 const container_lieux = document.querySelector("#container_lieux");
@@ -25,58 +18,49 @@ function LoadLocation() {
             tabData.forEach(location => {
                 container_lieux.innerHTML +=
                 `<div class="card">
-                <div class="card__text-container">
-                    <div class="location-name" data-url="${location.url}"  id="location-${location.id}">
-                        Name : ${location.name}
-                        <ul class="location_details">
-                            <li>Dimension : ${location.dimension}</li>
-                            <li>Type : ${location.type}</li>              
-                        </ul>
+                    <div class="card__text-container scroller">
+                        <div class="location-name" data-url="${location.url}"  id="location-${location.id}">
+                        <i class="fas fa-space-shuttle"></i> ${location.name} 
+                            <ul class="location_details">
+                                <li>Dimension : ${location.dimension}</li>
+                                <li>Type : ${location.type}</li>              
+                            </ul>
+                        </div>                        
+                        <div class="character_details"> </div>   
                     </div>
-                    
-                    <div class="character_details">   
-                </div>   
                 </div>`
 
                     let divLocation = document.querySelectorAll(".location-name");
+                   
                     divLocation.forEach(locationName => {
                        
                         locationName.addEventListener("click", (event) => {
                             fetch(event.target.dataset.url).then((resp) => {
-                              
                                 return resp.json()
-                            }).then((location) => {
-                           
+                            }).then((location) => {                          
                                 getCharacterDetails(location.residents, event.target);
                             })
 
-                            let clickScroll_details = event.target.querySelector(".location_details");
-
+                            let clickScroll_details = event.target.childNodes[3];       
                             let clickScroll_character = event.target.nextElementSibling;
-                            //let clickScroll_details = event.target.childNodes;
-
+                            
                             toggleClass(clickScroll_character, "character_details");
                             toggleClass(clickScroll_details, "location_details");
                         })
                     });
             });
 
-
             let typesRadio = document.querySelectorAll("input[type=radio]");
 
             for (let ind = 0; ind < typesRadio.length; ind++) {
                    
                 typesRadio[ind].addEventListener("change", (event) => {
-                    //console.log(tabData);
-
                         let newTabData;
                      if(event.target.value != "all"){
-                        newTabData = tabData.filter(data => data.type == `${event.target.id}`);
-                                            
+                        newTabData = tabData.filter(data => data.type == `${event.target.id}`);                            
                         } else {
                            newTabData = tabData;
-                     }
-        
+                     }        
                     //console.log(newTabData);
 
                     container_lieux.innerHTML = "";
@@ -84,55 +68,39 @@ function LoadLocation() {
                         
                         container_lieux.innerHTML +=
                             `<div class="card">
-                            <div class="card__text-container">
-                                <div class="location-name" data-url="${location.url}"  id="location-${location.id}">
-                                    Name : ${location.name}
-                                    <ul class="location_details">
-                                        <li>Dimension : ${location.dimension}</li>
-                                        <li>Type : ${location.type}</li>              
-                                    </ul>
-                                </div>
-                                
-                                <div class="character_details">   
-                            </div>   
+                                <div class="card__text-container scroller">
+                                    <div class="location-name" data-url="${location.url}"  id="location-${location.id}">
+                                    <i class="fas fa-space-shuttle"></i> ${location.name}
+                                        <ul class="location_details">
+                                            <li>Dimension : ${location.dimension}</li>
+                                            <li>Type : ${location.type}</li>              
+                                        </ul>
+                                    </div>
+                                    <div class="character_details"> </div>  
+                                </div> 
                             </div>`
 
                             document.querySelectorAll(".location-name").forEach((title, index) => {
                                 
                                 title.addEventListener("click", (event)=> {
-                                    //console.log(event);
     
                                         let listUriCharacter = newTabData[index].residents;
-                                        console.log(listUriCharacter);
-                                        
-                                        fetch(event.target.dataset.url).then((resp) => {
-                                            //console.log(uri)
+
+                                        fetch(event.target.dataset.url).then((resp) => {                                
                                             return resp.json()
                                         }).then((location) => {
-                                           // console.log(location);
-                                           
-                                            //getCharacterDetails(location.residents, event.target);
-                                        })
-                                    
-                                        let clickScroll_details = event.target.querySelector(".location_details");
+                                      })
                                         
-                                        //console.log(event);
-    
-                                        let clickScroll_character = event.target.parentNode.querySelector(".character_details");
-                                        
-            
-
-                                        console.log(clickScroll_character, clickScroll_details)
+                                        let clickScroll_details = event.target.childNodes[3];
+                                        let clickScroll_character = event.target.nextElementSibling;
+                                        console.log(event.target.nextElementSibling);
+                                                    
                                         toggleClass(clickScroll_character, "character_details");
                                         toggleClass(clickScroll_details, "location_details");
-    
+                       
                                         getCharacterDetails(listUriCharacter, event.target);
-                                  
-                                    //console.log(event)
                                 })
-
                             });                            
-
                     });
                 })
             }
@@ -154,20 +122,22 @@ function getCharacterDetails(listUriCharacter, divEp) {
         }).then((resp2) => {
             //console.log(resp2.id)
             reponse.push(resp2);
-            divEp.nextElementSibling.innerHTML += `<span> ${resp2.name} </span>`
+            divEp.nextElementSibling.innerHTML += 
+            `<div class="character">
+            <img src="${resp2.image}" alt="${resp2.name}">
+            <div class="characters_content">
+                <h2>${resp2.name}</h2>
+                <div class="details">${resp2.gender}</div>
+                <div class="details">${resp2.species}</div>
+               <div/> 
+            </div>
+            `
         })
     }
 }
 
-
 function toggleClass(elem, className) {
     elem.classList.toggle(className);
 }
-
-
-
-
-
-
 
 
